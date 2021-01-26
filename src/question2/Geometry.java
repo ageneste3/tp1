324a2fe4c3ce148fb39f172999af682d9edc8973
ageneste3 <alex.geneste3@gmail.com>
2021-01-26 21:12:59 +0100

    [func] implementation of Geometry class

--- src/question2/Geometry.java
+++ src/question2/Geometry.java
@@ -1,5 +1,37 @@
 package question2;
 
+import java.util.Objects;
+
 public class Geometry {
-    public String type;
+    private String type;
+    private String color;
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public static int surface(int i, int i1) {
+        return i * i1;
+    }
+
+    public static int perimeter(int i) {
+        return i * 4;
+    }
+
+    @Override
+    public String toString() {
+        return "Type:" + type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Geometry geometry = (Geometry) o;
+        return type.equals(geometry.type);
+    }
 }
